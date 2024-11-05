f6e9cd18eebf93d5bf11fee79fd3eba6b9f25cba
nikolasladic <nikola.sladic5@gmail.com>
2024-11-05 21:34:21 +0100

    sorting data from API

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const APIKey = "c89a66dd3f34858bd9f2e6dd41428f0b";
-const APIUrl = `https://api.openweathermap.org/data/2.5/weather?&units=metric&q=sibenik`;
+// const APIUrl = `https://api.openweathermap.org/data/2.5/weather?&units=metric&q=sibenik`;
+
+const APIUrl = `https://api.openweathermap.org/data/2.5/forecast?q=sibenik&units=metric`;
 
 function capitalizeWords(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
@@ -7,21 +9,42 @@ function capitalizeWords(str) {
 
 async function checkWeather() {
   const res = await fetch(APIUrl + `&appid=${APIKey}`);
-  let data = await res.json();
+  const data = await res.json();
 
-  console.log(data);
+  const sortedData = groupByDay(data.list);
+  console.log(sortedData);
 
-  document.querySelector(".cur-location").innerHTML = data.name;
-  document.querySelector(".temp-details").innerHTML =
-    Math.round(data.main.temp) + "°C";
-  document.querySelector(".weather-description").innerHTML = capitalizeWords(
-    data.weather[0].description
-  );
-  document.querySelector(".feels-like").innerHTML =
-    "Feels like: " + Math.round(data.main.feels_like) + " °C";
-  document.querySelector(".humidity").innerHTML =
-    "Humidity: " + Math.round(data.main.humidity) + "%";
-  document.querySelector(".wind").innerHTML =
-    "Wind: " + Math.round(data.wind.speed) + " km/h";
+  console.log(data.list);
+
+  // document.querySelector(".cur-location").innerHTML = data.name;
+  // document.querySelector(".temp-details").innerHTML =
+  //   Math.round(data.main.temp) + "°C";
+  // document.querySelector(".weather-description").innerHTML = capitalizeWords(
+  //   data.weather[0].description
+  // );
+  // document.querySelector(".feels-like").innerHTML =
+  //   "Feels like: " + Math.round(data.main.feels_like) + " °C";
+  // document.querySelector(".humidity").innerHTML =
+  //   "Humidity: " + Math.round(data.main.humidity) + "%";
+  // document.querySelector(".wind").innerHTML =
+  //   "Wind: " + Math.round(data.wind.speed) + " km/h";
 }
+
+function groupByDay(data) {
+  return data.reduce((acc, item) => {
+    // Convert timestamp to date string (e.g., "2024-11-01")
+    const date = new Date(item.dt * 1000).toISOString().split("T")[0];
+
+    // Initialize array for the date if it doesn't exist
+    if (!acc[date]) {
+      acc[date] = [];
+    }
+
+    // Push the item to the corresponding date array
+    acc[date].push(item);
+
+    return acc;
+  }, {});
+}
+
 checkWeather();
